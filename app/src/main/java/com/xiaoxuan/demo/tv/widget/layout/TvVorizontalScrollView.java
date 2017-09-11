a41f26a6ec3167a280f7a8da54da65be8a0a959d
xiaoxuan <xiaoxuan@xiaoxuandeMacBook-Pro.local>
2017-09-11 10:47:52 +0800

    [bug]修复水平不能移动到底的问题

---
+++ app/src/main/java/com/xiaoxuan/demo/tv/widget/layout/TvVorizontalScrollView.java
@@ -0,0 +1,75 @@
+package com.xiaoxuan.demo.tv.widget.layout;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+import com.xiaoxuan.demo.androidtvdemoxx.R;
+
+public class TvVorizontalScrollView extends ScrollView
+{
+    
+    public TvVorizontalScrollView(Context context)
+    {
+        super(context, null, 0);
+    }
+    
+    public TvVorizontalScrollView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs, 0);
+    }
+    
+    public TvVorizontalScrollView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+    
+    @Override
+    protected int computeScrollDeltaToGetChildRectOnScreen(Rect rect)
+    {
+        if (getChildCount() == 0)
+            return 0;
+        int height = getHeight();
+        int screenTop = getScrollY();
+        int screenBottom = screenTop + height;
+        int fadingEdge = this.getResources().getDimensionPixelSize(R.dimen.fading_edge);
+        if (rect.top > 0)
+        {
+            screenTop += fadingEdge;
+        }
+        if (rect.bottom < getChildAt(0).getHeight())
+        {
+            screenBottom -= fadingEdge;
+        }
+        
+        int scrollYDelta = 0;
+        if (rect.bottom > screenBottom && rect.top > screenTop)
+        {
+            if (rect.height() > height)
+            {
+                scrollYDelta += (rect.top - screenTop);
+            }
+            else
+            {
+                scrollYDelta += (rect.bottom - screenBottom);
+            }
+            int bottom = getChildAt(0).getBottom();
+            int distanceToBottom = bottom - screenBottom;
+            scrollYDelta = Math.min(scrollYDelta, distanceToBottom);
+        }
+        else if (rect.top < screenTop && rect.bottom < screenBottom)
+        {
+            if (rect.height() > height)
+            {
+                scrollYDelta -= (screenBottom - rect.bottom);
+            }
+            else
+            {
+                scrollYDelta -= (screenTop - rect.top);
+            }
+            scrollYDelta = Math.max(scrollYDelta, -getScrollY());
+        }
+        return scrollYDelta;
+    }
+}
---
+++ app/src/main/java/com/xiaoxuan/demo/tv/widget/view/TvHorizontalScrollView.java
@@ -0,0 +1,84 @@
+package com.xiaoxuan.demo.tv.widget.view;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+import com.xiaoxuan.demo.androidtvdemoxx.R;
+
+public class TvHorizontalScrollView extends HorizontalScrollView
+{
+    
+    public TvHorizontalScrollView(Context context)
+    {
+        this(context, null, 0);
+    }
+    
+    public TvHorizontalScrollView(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+    
+    public TvHorizontalScrollView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+    
+    @Override
+    protected int computeScrollDeltaToGetChildRectOnScreen(Rect rect)
+    {
+        if (getChildCount() == 0)
+            return 0;
+        
+        int width = getWidth();
+        int screenLeft = getScrollX();
+        int screenRight = screenLeft + width;
+        
+        int fadingEdge = this.getResources().getDimensionPixelSize(R.dimen.fading_edge);
+        
+        if (rect.left > 0)
+        {
+            screenLeft += fadingEdge;
+        }
+        
+        if (rect.right < getChildAt(0).getWidth())
+        {
+            screenRight -= fadingEdge;
+        }
+        
+        int scrollXDelta = 0;
+        
+        if (rect.right > screenRight && rect.left > screenLeft)
+        {
+            if (rect.width() > width)
+            {
+                scrollXDelta += (rect.left - screenLeft);
+            }
+            else
+            {
+                scrollXDelta += (rect.right - screenRight);
+            }
+            
+            int right = getChildAt(0).getRight();
+            int distanceToRight = right - screenRight;
+            scrollXDelta = Math.min(scrollXDelta, distanceToRight);
+            
+        }
+        else if (rect.left < screenLeft && rect.right < screenRight)
+        {
+            if (rect.width() > width)
+            {
+                scrollXDelta -= (screenRight - rect.right);
+            }
+            else
+            {
+                scrollXDelta -= (screenLeft - rect.left);
+            }
+            
+            scrollXDelta = Math.max(scrollXDelta, -getScrollX());
+        }
+        return scrollXDelta;
+    }
+    
+}
